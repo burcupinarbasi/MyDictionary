1d156603a82d472a8b4953eb10d03f2e5fbd09f3
BURCU PINARBASI <brcupnrbs@gmail.com>
2021-02-20 17:12:16 +0300

    first commit

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] _keys;
+        V[] _values;
+
+        public MyDictionary()
+        {
+            _keys = new K[0];
+            _values = new V[0];
+        }
+        public void Add(K key, V value)
+        {
+           
+            K[] TempKeys = _keys;
+            V[] TempValues = _values;
+            _keys = new K[_keys.Length + 1];
+            _values = new V[_values.Length +1];
+            for (int i = 0; i < TempKeys.Length; i++)
+            {
+                _keys[i] = TempKeys[i];
+                _values[i] = TempValues[i];
+
+            }
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+
+        }
+
+        public int Count 
+        { get { return _keys.Length; } 
+        }
+    }
+        
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+
+            myDictionary.Add(123, "Burcu");
+            myDictionary.Add(124, "Ayse");
+            myDictionary.Add(125, "Bahar");
+            myDictionary.Add(126, "Cansu");
+
+
+            int sayi = myDictionary.Count;
+            Console.WriteLine("eleman sayisi"+sayi);
+
+        }
+    }
+}
